eab69fa25b0cb8d77bd5efbfbbb1aaf828ad0f8e
Wayne Kiprotich <wanekip123@icloud.com>
2025-12-19 14:17:18 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
+//Creating a function that merges usernames into one
 
+//
+require('datejs');
 
+//function to combine users
+function combineUsers(...args) {
+
+  //Initialize the Return Object
+  const combinedObject = { 
+    users: []
+  };
+
+  //Loop through the arguments
+  for (let i = 0; i < args.length; i++) {
+    combinedObject.users = [...combinedObject.users, ...args[i]];
+  }
+
+  //Adding date
+  combinedObject.merge_date = Date.today().toString('M/d/yyyy');
+
+  return combinedObject;
+}
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
